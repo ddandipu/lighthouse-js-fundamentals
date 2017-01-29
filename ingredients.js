7f5906a8219bbadb34b3a53d8acdc72042c141c8
ddandipu <andiputra.dimas@hotmail.com>
2017-01-29 05:09:34 +0000

    added problem solution

---
+++ ingredients.js
@@ -0,0 +1,13 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var i=0;
+  while (i<ingredients.length){
+    console.log(ingredients[i]);
+    i++;
+  }
+
+for(var o=0; o<ingredients.length; o++){
+  console.log(ingredients[o]);
+}
+for(var p=ingredients.length-1; p>=0; p--){
+  console.log(ingredients[p]);
+}
